bf7eea766590caaf8a911e42e6a64e5219fe06c8
Andrey <trishichev@gmail.com>
2021-05-06 18:13:08 +0300

    Update bot
    add smile

--- bot.py
+++ bot.py
@@ -20,6 +20,17 @@ async def start_command(message: types.Message):
 
 @dp.message_handler()
 async def get_weather(message: types.Message):
+
+    code_to_smile = {
+        "Clear": "Ясно \U00002600",
+        "Clouds": "Облачно \U00002601",
+        "Rain": "Дождь \U00002614",
+        "Drizzle": "Дождь \U00002614",
+        "Thunderstorm": "Гроза \U000026A1",
+        "Snow": "Снег \U0001F328",
+        "Mist": "Туман \U0001F32B"
+    }
+
     try:
         request = requests.get(
             f'https://api.openweathermap.org/data/2.5/weather?q={message.text}&appid={open_weather_token}&units=metric')
@@ -27,6 +38,11 @@ async def get_weather(message: types.Message):
 
         city = data['name']
         weather = data['main']['temp']
+        weather_description = data["weather"][0]["main"]
+        if weather_description in code_to_smile:
+            wd = code_to_smile[weather_description]
+        else:
+            wd = "Посмотри в окно, не пойму что там за погода!"
         humidity = data['main']['humidity']
         pressure = data['main']['pressure']
         wind_speed = data['wind']['speed']
@@ -36,7 +52,7 @@ async def get_weather(message: types.Message):
             data['sys']['sunset'])
         await message.reply(
             f'|***{datetime.datetime.now().strftime("%Y-%m-%d %H:%M")}***|\n'
-            f'Погода в городе {city}\nТемпература: {weather}°C\nВлажность: {humidity}%\n'
+            f'Погода в городе {city}\nТемпература: {weather}°C {wd}\nВлажность: {humidity}%\n'
             f'Давление: {pressure}мм рт.ст\nВетер: {wind_speed} м/с\n'
             f'Восход солнца: {sunrise_timetamp}\n'
             f'Закат солнца: {sunset_timetamp}\n'
